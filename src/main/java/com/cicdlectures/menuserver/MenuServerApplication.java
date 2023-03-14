5ea67e0d9d778ed3d6350645e7bb0e5e63b3d99e
Jonathan Boris Ouedraogo <40577361+jonathanboris@users.noreply.github.com>
2023-03-14 16:50:40 +0100

    Update src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MenuServerApplication
+public class MenuServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MenuServerApplication.class, args);
